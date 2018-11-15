b963ec4f5f2081c049d95492c8b62145ca38171e
EC2 Default User <ec2-user@ip-172-31-89-237.ec2.internal>
2018-11-15 00:19:31 +0000

    created api service calls for workload module

--- src/app/service/workload.service.ts
+++ src/app/service/workload.service.ts
@@ -1,12 +1,32 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-//import {User} from "../model/user.model";
+import {workload} from "../model/workload.model";
 
 @Injectable()
 export class WorkloadService {
   constructor(private http: HttpClient) { }
-  baseUrl: string = 'http://back-end-serer-env2.uyvzpgtkte.us-east-1.elasticbeansta$';
+  baseUrl: string = 'http://back-end-serer-env2.uyvzpgtkte.us-east-1.elasticbeanstalk.com:8081/aapi/Workloads';
 
+  getWorkloads () {
+    return this.http.get<Workloads[]>(this.baseUrl);
+  }
+
+  getWorkloadById(id: string) {
+    return this.http.get<Workloads>(this.baseUrl + '/' + id);
+  }
+
+  createWorkload(workload: Workloads) {
+    return this.http.post(this.baseUrl, workload);
+  }
+
+  //adjust update
+  updateWorkload(workload: Workloads) {
+    return this.http.put(this.baseUrl + '/' + workload.id, workload);
+  }
+
+  deleteWorkload(id: string) {
+    return this.http.delete(this.baseUrl + '/' + id);
+  }
 
 
 }
